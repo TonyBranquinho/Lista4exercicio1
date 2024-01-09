480f43ed45de8db36129d4127f9393bdb0aeff7d
Tony <antony.branquinho@gmail.com>
2024-01-09 15:13:53 -0300

    basico

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Digite um valor inteiro.");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                if (i % 2 != 0) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+    }
+}
